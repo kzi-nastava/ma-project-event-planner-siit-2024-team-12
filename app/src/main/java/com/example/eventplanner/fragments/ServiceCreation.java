f2c99fe774053e7add5e3cd6ea4440b8f2cbb3b3
draganjordanovic <dragan.jjj.03@gmail.com>
2024-11-08 20:55:39 +0100

    Implemented floating button navigation to service creation fragment

--- app/src/main/java/com/example/eventplanner/fragments/ServiceCreation.java
+++ app/src/main/java/com/example/eventplanner/fragments/ServiceCreation.java
@@ -3,33 +3,30 @@ package com.example.eventplanner.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.eventplanner.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ServiceManagement#newInstance} factory method to
+ * Use the {@link ServiceCreation#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ServiceManagement extends Fragment {
+public class ServiceCreation extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private FloatingActionButton fabCreateService;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public ServiceManagement() {
+    public ServiceCreation() {
         // Required empty public constructor
     }
 
@@ -39,11 +36,11 @@ public class ServiceManagement extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ServiceManagement.
+     * @return A new instance of fragment ServiceCreation.
      */
     // TODO: Rename and change types and number of parameters
-    public static ServiceManagement newInstance(String param1, String param2) {
-        ServiceManagement fragment = new ServiceManagement();
+    public static ServiceCreation newInstance(String param1, String param2) {
+        ServiceCreation fragment = new ServiceCreation();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,18 +61,6 @@ public class ServiceManagement extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView =  inflater.inflate(R.layout.fragment_service_management, container, false);
-        fabCreateService = rootView.findViewById(R.id.floating_action_button);
-        fabCreateService.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragment_container, new ServiceCreation());
-                transaction.addToBackStack(null);
-                transaction.commit();
-            }
-        });
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment_service_creation, container, false);
     }
 }
