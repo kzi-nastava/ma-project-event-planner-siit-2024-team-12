80395bf4962073f239258dd1bb1d2656bd2a4665
draganjordanovic <dragan.jjj.03@gmail.com>
2024-11-13 17:16:00 +0100

    Created search bar, title and create button for service menagement

--- app/src/main/java/com/example/eventplanner/fragments/ServiceManagement.java
+++ app/src/main/java/com/example/eventplanner/fragments/ServiceManagement.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.eventplanner.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -23,7 +24,7 @@ public class ServiceManagement extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private FloatingActionButton fabCreateService;
+    private ImageButton imgButton;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -65,8 +66,8 @@ public class ServiceManagement extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView =  inflater.inflate(R.layout.fragment_service_management, container, false);
-        fabCreateService = rootView.findViewById(R.id.floating_action_button);
-        fabCreateService.setOnClickListener(new View.OnClickListener() {
+        imgButton = rootView.findViewById(R.id.imageButton);
+        imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
