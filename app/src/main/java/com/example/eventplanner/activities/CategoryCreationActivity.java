62214ac0dd361debe9a7e187a57a6c59de8a587c
draganjordanovic <dragan.jjj.03@gmail.com>
2024-11-15 11:57:55 +0100

    Implemented category creation form

--- app/src/main/java/com/example/eventplanner/activities/CategoryCreationActivity.java
+++ app/src/main/java/com/example/eventplanner/activities/CategoryCreationActivity.java
@@ -1,6 +1,9 @@
 package com.example.eventplanner.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +20,28 @@ public class CategoryCreationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_category_creation);
+        Button backButton = findViewById(R.id.backCatCreate);
+        Button submitButton = findViewById(R.id.newCatSubmit);
+        String CategoryCreatedMessage = getString(R.string.category_created);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        backButton.setOnClickListener(v -> {
+            setResult(RESULT_CANCELED);
+            finish(); // Zatvara aktivnost i vraća se na fragment
+        });
+
+        submitButton.setOnClickListener(v -> {
+            Toast.makeText(this, CategoryCreatedMessage, Toast.LENGTH_SHORT).show();
+            Intent resultIntent = new Intent();
+            setResult(RESULT_OK, resultIntent);
+            finish(); // Zatvara aktivnost i vraća na fragment
+        });
+
     }
 }
