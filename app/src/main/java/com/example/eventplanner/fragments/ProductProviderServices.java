5eaa5d9f8e4eb9eadf654466f642b7db62903e26
draganjordanovic <dragan.jjj.03@gmail.com>
2024-11-15 17:22:23 +0100

    Created service edit activity

--- app/src/main/java/com/example/eventplanner/fragments/ProductProviderServices.java
+++ app/src/main/java/com/example/eventplanner/fragments/ProductProviderServices.java
@@ -1,7 +1,12 @@
 package com.example.eventplanner.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.eventplanner.R;
+import com.example.eventplanner.activities.ServiceEditActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +22,8 @@ import com.example.eventplanner.R;
  * create an instance of this fragment.
  */
 public class ProductProviderServices extends Fragment {
+
+    private ActivityResultLauncher<Intent> activityResultLauncher;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,12 +63,29 @@ public class ProductProviderServices extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Obradi rezultat ovde ako je potrebno
+                        Intent data = result.getData();
+                        // Na primer, možeš pročitati podatke iz Intent-a ovde
+                    }
+                }
+        );
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_provider_services, container, false);
+        View view = inflater.inflate(R.layout.fragment_product_provider_services, container, false);
+        AppCompatButton serviceEditButton = view.findViewById(R.id.service_edit);
+        serviceEditButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ServiceEditActivity.class);
+            activityResultLauncher.launch(intent);
+        });
+        return view;
     }
 }
