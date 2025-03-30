66090e3ddf625bbeed97bb49ff91aded950da1b7
trivunic.a <trivunic02@gmail.com>
2025-03-30 21:44:34 +0200

    Refactored event type edit activity

--- app/src/main/java/com/example/eventplanner/dto/eventtype/GetEventTypeDTO.java
+++ app/src/main/java/com/example/eventplanner/dto/eventtype/GetEventTypeDTO.java
@@ -2,26 +2,32 @@ package com.example.eventplanner.dto.eventtype;
 
 import java.util.List;
 
+
+
 public class GetEventTypeDTO {
-    private String id;
+    private Long id;
     private String name;
     private String description;
     private List<String> suggestedCategoryNames;
-    private boolean active;
+    private Boolean isActive;
     private boolean isExpanded;
 
-    public GetEventTypeDTO() {}
 
-    public GetEventTypeDTO(String id, String name, String description, boolean active) {
+
+
+    public GetEventTypeDTO() { super(); }
+
+    public GetEventTypeDTO(Long id, String name, String description, Boolean isActive, List<String> suggestedCategoryNames) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.active = active;
+        this.isActive = isActive;
+        this.suggestedCategoryNames = suggestedCategoryNames;
         this.isExpanded = false;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
@@ -30,23 +36,22 @@ public class GetEventTypeDTO {
     public void setDescription(String description) { this.description = description; }
 
     public List<String> getSuggestedCategoryNames() { return suggestedCategoryNames; }
-    public void setSuggestedCategoryNames(List<String> categoryNames) { this.suggestedCategoryNames = categoryNames; }
+    public void setSuggestedCategoryNames(List<String> suggestedCategoryNames) {this.suggestedCategoryNames = suggestedCategoryNames; }
 
-    public boolean getActive() { return active; }
-    public void setActive(boolean active) { this.active = active; }
+    public Boolean getIsActive() { return isActive; }
+    public void setIsActive(Boolean isActive) { this.isActive = isActive; }
 
-    public boolean isExpanded() { return isExpanded; }
-    public void setExpanded(boolean expanded) { this.isExpanded = expanded; }
+    public boolean getIsExpanded() { return isExpanded; }
+    public void setIsExpanded(boolean isExpanded) { this.isExpanded = isExpanded; }
 
     @Override
     public String toString() {
         return "GetEventTypeDTO{" +
                 "id=" + id +
-                ", name=" + name + '\'' +
-                ", description=" + description + '\'' +
-                ", suggested=" + suggestedCategoryNames + '\'' +
-                ", active=" + active +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", suggestedCategoryNames=" + suggestedCategoryNames +
+                ", isActive=" + isActive +
                 '}';
     }
-
 }
