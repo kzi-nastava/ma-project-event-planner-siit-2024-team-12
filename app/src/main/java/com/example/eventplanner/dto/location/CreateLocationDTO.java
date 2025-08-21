b6a18a588b52e6ca7d9951015b6eaf97356d3b93
trivunic.a <trivunic02@gmail.com>
2025-08-21 23:48:40 +0200

    Added date picker to event forms and removed exit button image view from other forms

--- app/src/main/java/com/example/eventplanner/dto/location/CreateLocationDTO.java
+++ app/src/main/java/com/example/eventplanner/dto/location/CreateLocationDTO.java
@@ -29,4 +29,16 @@ public class CreateLocationDTO implements Serializable {
     public String getCountry() {return country;}
     public void setCountry(String country) {this.country = country;}
 
+
+    @Override
+    public String toString() {
+        return "CreateLocationDTO{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+
 }
